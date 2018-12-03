ab186de5dea74de1942909458b30ea14c937b063
Andrew MacRobert <andrew.macrobert@mequilibrium.com>
2018-12-03 17:13:19 -0500

    admin work

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
     }
 }
